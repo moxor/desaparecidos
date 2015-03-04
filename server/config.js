78577bcf008a359890f2d390dedb5a6b640c211f
moxor <aaron@kimmigs.de>
2015-03-04 20:57:06 +0100

    Added some styles for the desaparecidos_window

---
+++ server/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nanu on 08.01.15.
+ */
+Meteor.startup(function () {
+    process.env.MAIL_URL = 'smtp://postmaster%40nanu-c.org:0f62cd56ab5553e132d38daaf42170c3@smtp.mailgun.org:587';
+});
